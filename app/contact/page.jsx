33deb331f7576193d7ad6a2d93c9c27926668264
MohammadKerdasi <mohammadkerdasi003@gmail.com>
2024-11-18 18:00:26 +0200

    edit 6

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
       <div className="flex flex-col xl:flex-row gap-[30px]">
         <div className="xl:h-[54%] order-2 xl:order-none">
           <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-            <h3 className="text-4xl text-accent">Let's work together</h3>
+            <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
               Eos asperiores eaque non, nulla quas id sunt numquam doloremque 
               impedit voluptatibu
--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Front-end Developer</span>
             <h1 className="h1">
-              Hello I am
+              Hello I&apos;m
               <br /> <span className="text-accent">Mohammad Kerdasi</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
@@ -20,25 +20,28 @@ const Home = () => {
               compelling, user-centric web applications.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button 
-              variant="outline"
-              size="lg"
-              className="uppercase flex items-center gap-2"
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
               >
                 <span>Download CV</span>
-              <FiDownload className="text-xl" />
+                <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0 ">
-                <Social containerStyles="flex gap-6" iconStyle="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent/90 hover:text-primary hover:transition-all duration-500"/>
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyle="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent/90 hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
           <div className=" order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo/>
+            <Photo />
           </div>
         </div>
       </div>
-      <Stats/>
+      <Stats />
     </section>
   );
 };
